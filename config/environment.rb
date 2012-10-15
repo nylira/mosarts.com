e334abc4110e4d0d039a1bf3b1ebdc247fa7c56c
Peng Zhong <peng@nylira.com>
2012-10-15 20:43:04 +0900

    init

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Mosarts::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mosarts::Application.config.secret_token = '90c2318d14e364ae52229355975cc61560d125a17f08fc9353a84047e0287017271351a4c54a942c019ddfca8232b1a844720493884f9ec9ad90ac0524832d53'
